81a54221bf3e82447a528ddb947b57aa77716dab
dcontrer83 <dcontrer83@yahoo.com>
2022-07-28 03:54:33 -0700

    Update email and GitHub links

--- src/generateHtml.js
+++ src/generateHtml.js
@@ -9,13 +9,19 @@ function generateHtmlManager(manager) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
         <link rel="stylesheet" href="./style.css"/>
-        <title>Document</title>
+        <title>Team Profile Generator</title>
     </head>
 
     <body>
+      <div class="jumbotron jumbotron-fluid jumbotronCSS">
+        <div class="container">
+          <h1 class="display-4">My Team</h1>
+        </div>
+      </div>
+
       <div id="allCards">
 
-        <div class="card" style="width: 18rem;">
+        <div class="card">
           <div class="card-header">
             Name: ${manager.name}
             <br>
@@ -23,7 +29,7 @@ function generateHtmlManager(manager) {
           </div>
           <ul class="list-group list-group-flush">
             <li class="list-group-item">ID: ${manager.id}</li>
-            <li class="list-group-item">Email: ${manager.email}</li>
+            <li class="list-group-item">Email: <a href="mailto:${manager.email}">${manager.email}</a></li>
             <li class="list-group-item">Office number: ${manager.officeNumber}</li>
           </ul>
         </div>
@@ -32,7 +38,7 @@ function generateHtmlManager(manager) {
 
 function generateHtmlEngineer(engineer) {
   return `          
-        <div class="card" style="width: 18rem;">
+        <div class="card">
           <div class="card-header">
             Name: ${engineer.name}
             <br>
@@ -40,8 +46,8 @@ function generateHtmlEngineer(engineer) {
           </div>
           <ul class="list-group list-group-flush">
             <li class="list-group-item">ID: ${engineer.id}</li>
-            <li class="list-group-item">Email: ${engineer.email}</li>
-            <li class="list-group-item">GitHub: ${engineer.github}</li>
+            <li class="list-group-item">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></li>
+            <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.github}" target="_blank" rel="noopener noreferrer">${engineer.github} GitHub</a></li>
           </ul>
         </div>
         `
@@ -49,7 +55,7 @@ function generateHtmlEngineer(engineer) {
 
 function generateHtmlIntern(intern) {
   return `
-        <div class="card" style="width: 18rem;">
+        <div class="card">
           <div class="card-header">
             Name: ${intern.name}
             <br>
@@ -57,7 +63,7 @@ function generateHtmlIntern(intern) {
           </div>
           <ul class="list-group list-group-flush">
             <li class="list-group-item">ID: ${intern.id}</li>
-            <li class="list-group-item">Email: ${intern.email}</li>
+            <li class="list-group-item">Email: <a href="mailto:${intern.email}">${intern.email}</a></li>
             <li class="list-group-item">School: ${intern.school}</li>
           </ul>
         </div>
